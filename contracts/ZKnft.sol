f0e2a9efe94f106f640923835802cd4c4aa27d28
eerkaijun <kaijuneer@gmail.com>
2022-08-19 18:58:37 +0800

    tie an identity to an address

--- contracts/ZKnft.sol
+++ contracts/ZKnft.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ZKnft is ERC721, Ownable {
+contract ZKnft is ERC721URIStorage, Ownable {
 
     uint256 public currentTokenID = 0;
 
     constructor() ERC721("ZK NFT", "ZID") {}
 
-    function mintZKID(address account) public onlyOwner {
+    function mintZKID(address account, string memory tokenURI) public onlyOwner {
         _mint(account, currentTokenID);
+        _setTokenURI(currentTokenID, tokenURI);
         currentTokenID++;
     }
 
